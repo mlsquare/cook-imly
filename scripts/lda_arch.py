4864ea05a92d1d84dd78fbcb7596fe8bf5061358
dhavala <soma.dhavala@gmail.com>
2019-09-27 08:22:01 +0530

    lda sample

--- scripts/lda_arch.py
+++ scripts/lda_arch.py
@@ -1,63 +1,10 @@
-__author__ = 'tchaton'
+# sourced from https://github.com/tchaton/DeepLDA
+# and which inturn is inspired from https://github.com/VahidooX/DeepLDA.
+# refs
+# https://arxiv.org/pdf/1903.11240.pdf
+# https://arxiv.org/pdf/1906.02590.pdf 
 
-'''
-This code has been taken from https://github.com/VahidooX/DeepLDA.
-It is written in python using Theano Library.
-import theano.tensor as T
-import theano
-import numpy as np
-from theano.compile.ops import as_op
-@as_op(itypes=[theano.tensor.ivector],
-       otypes=[theano.tensor.ivector])
-def numpy_unique(a):
-    return np.unique(a)
-def lda_loss(n_components, margin):
-    """
-    The main loss function (inner_lda_objective) is wrapped in this function due to
-    the constraints imposed by Keras on objective functions
-    """
-    def inner_lda_objective(y_true, y_pred):
-        """
-        It is the loss function of LDA as introduced in the original paper.
-        It is adopted from the the original implementation in the following link:
-        https://github.com/CPJKU/deep_lda
-        Note: it is implemented by Theano tensor operations, and does not work on Tensorflow backend
-        """
-        r = 1e-4
-        # init groups
-        yt = T.cast(y_true.flatten(), "int32")
-        groups = numpy_unique(yt)
-        def compute_cov(group, Xt, yt):
-            Xgt = Xt[T.eq(yt, group).nonzero()[0], :]
-            Xgt_bar = Xgt - T.mean(Xgt, axis=0)
-            m = T.cast(Xgt_bar.shape[0], 'float32')
-            return (1.0 / (m - 1)) * T.dot(Xgt_bar.T, Xgt_bar)
-        # scan over groups
-        covs_t, updates = theano.scan(fn=compute_cov, outputs_info=None,
-                                      sequences=[groups], non_sequences=[y_pred, yt])
-        # compute average covariance matrix (within scatter)
-        Sw_t = T.mean(covs_t, axis=0)
-        # compute total scatter
-        Xt_bar = y_pred - T.mean(y_pred, axis=0)
-        m = T.cast(Xt_bar.shape[0], 'float32')
-        St_t = (1.0 / (m - 1)) * T.dot(Xt_bar.T, Xt_bar)
-        # compute between scatter
-        Sb_t = St_t - Sw_t
-        # cope for numerical instability (regularize)
-        Sw_t += T.identity_like(Sw_t) * r
-        # return T.cast(T.neq(yt[0], -1), 'float32')*T.nlinalg.trace(T.dot(T.nlinalg.matrix_inverse(St_t), Sb_t))
-        # compute eigenvalues
-        evals_t = T.slinalg.eigvalsh(Sb_t, St_t)
-        # get eigenvalues
-        top_k_evals = evals_t[-n_components:]
-        # maximize variance between classes
-        # (k smallest eigenvalues below threshold)
-        thresh = T.min(top_k_evals) + margin
-        top_k_evals = top_k_evals[(top_k_evals <= thresh).nonzero()]
-        costs = T.mean(top_k_evals)
-        return -costs
-    return inner_lda_objective
-'''
+
 
 import tensorflow as tf
 from sklearn import svm
@@ -85,7 +32,7 @@ def svm_classify(x_train, y_train, x_test, y_test, C):
     return [train_acc, test_acc]
 
 
-def lda_loss(n_components, margin):
+def lda_loss(n_components, margin,method='raleigh_coeff'):
     """
     The main loss function (inner_lda_objective) is wrapped in this function due to
     the constraints imposed by Keras on objective functions
@@ -99,18 +46,10 @@ def lda_loss(n_components, margin):
         Note: it is implemented by Theano tensor operations, and does not work on Tensorflow backend
         """
         r = 1e-4
-        print("in lda inner loop")
-        print(y_true)
-        print(y_pred)
-
-        # init groups
-        # yt = tf.cast(tf.contrib.layers.flatten(y_true), tf.float32)
-        # indexes = tf.argmax (y_true, axis=-1)
         locations = tf.where(tf.equal(y_true, 1))
         indices = locations[:, 1]
         y, idx = tf.unique(indices)
 
-        print(locations)
 
         def fn(unique, indexes, preds):
             u_indexes = tf.where(tf.equal(unique, indexes))
@@ -136,40 +75,39 @@ def lda_loss(n_components, margin):
         Sb_t = St_t - Sw_t
 
         # cope for numerical instability (regularize)
-
         Sw_t += tf.eye(dim) * r
 
         ''' START : COMPLICATED PART WHERE TENSORFLOW HAS TROUBLE'''
-
         #cho = tf.eye(dim)
         # look at page 383
         # http://perso.ens-lyon.fr/patrick.flandrin/LedoitWolf_JMA2004.pdf
-        
-        r = 100
-        cho = tf.cholesky(St_t + tf.eye(dim) * r)
-        inv_cho = tf.matrix_inverse(cho)
-        evals_t = tf.linalg.eigvalsh(inv_cho * Sb_t * tf.transpose(inv_cho + tf.eye(dim) * r))  # Sb_t, St_t # SIMPLIFICATION OF THE EQP USING cholesky
-        #evals_t = tf.abs(tf.linalg.eigvalsh(tf.matrix_inverse(Sw_t) * Sb_t )) # INVERSED EQUATION
-        top_k_evals = evals_t[-n_components:]
 
-        ''' END : COMPLICATED PART WHERE TENSORFLOW HAS TROUBLE'''
+        if method == 'raleigh_coeff':
+            # minimize the -ve of Raleigh coefficient
+            r = 1e-4
+            cho = tf.cholesky(St_t + tf.eye(dim) * r)
+            inv_cho = tf.matrix_inverse(cho)
+            evals_t = tf.linalg.eigvalsh(tf.transpose(inv_cho) * Sb_t * inv_cho)  # Sb_t, St_t # SIMPLIFICATION OF THE EQP USING cholesky    
+            top_k_evals = evals_t[-n_components:]
 
-        # index_max = tf.argmax(top_k_evals, 0)
-        # thresh_max = top_k_evals[index_max] - margin
+            index_min = tf.argmin(top_k_evals, 0)
+            thresh_min = top_k_evals[index_min] + margin
+            mask_min = top_k_evals < thresh_min
+            cost_min = tf.boolean_mask(top_k_evals, mask_min)
+            cost  = -tf.reduce_mean(cost_min)
 
-        index_min = tf.argmin(top_k_evals, 0)
-        thresh_min = top_k_evals[index_min] + margin
-        # thresh = tf.contrib.distributions.percentile(top_k_evals, 33) # TRY TO TAKE A PERCENTILE INSTEAD THAN A THRESOLD
-        # mask_max = top_k_evals > thresh_max
-        mask_min = top_k_evals < thresh_min
-
-        # cost_max = tf.boolean_mask(top_k_evals, mask_max)
-        cost_min = tf.boolean_mask(top_k_evals, mask_min)
-        cost = -tf.reduce_mean(cost_min)
-        
-        cost = tf.math.divide(tf.linalg.trace(Sb_t),tf.linalg.trace(Sw_t))
+        elif method == 'trace_ratio':
+            # minimize the -ve of ratio of trace of betwwen to witin scatter
+            cost = -tf.math.divide(tf.linalg.trace(Sb_t),tf.linalg.trace(Sw_t))
+        elif method == 'trace_diff':
+            # minimize with variation, maximze between variation
+            cost = tf.linalg.trace(Sw_t)-tf.linalg.trace(Sb_t)
+        else:
+            # minimize within variation
+            cost = tf.linalg.trace(Sw_t)
 
         return cost
+
 
     return inner_lda_objective
 
@@ -232,6 +170,10 @@ def lda_trace_loss(n_components, margin):
 
     return inner_lda_trace_objective
 
+
+
+
+
 import numpy as np
 from keras.datasets import mnist
 from keras.optimizers import Adam
@@ -267,7 +209,7 @@ if __name__ == '__main__':
     save_to = './new_features.gz'
 
     # the size of the new space learned by the model (number of the new features)
-    outdim_size = 10
+    outdim_size = 20
 
     # the parameters for training the network
     epoch_num = 10
@@ -305,21 +247,24 @@ if __name__ == '__main__':
 
     ip_layer = Input(shape=(input_dim,))
     lda_layer = Dense(outdim_size, name='lda', activation='linear', kernel_regularizer=l2(reg_par))(ip_layer) 
-    #op_layer = Dense(nr_classes,name='output' ,activation='softmax')(lda_layer)
-    op_layer = Dense(nr_classes,name='output' ,activation='softmax')(ip_layer)
+    op_layer = Dense(nr_classes,name='output' ,activation='softmax')(lda_layer)
+    #op_layer = Dense(nr_classes,name='output' ,activation='softmax')(ip_layer)
 
     model = Model(inputs=ip_layer, outputs=[lda_layer,op_layer])
-
-    losses = {'lda':lda_trace_loss(n_components, margin),'output':'categorical_crossentropy'}
-    #losses = {'lda':lda_loss(n_components, margin)}
-    #losses = {'output':'categorical_crossentropy'}
-
+    losses = {'lda':lda_loss(n_components, margin),'output':'categorical_crossentropy'}
     model.compile(loss=losses, optimizer='rmsprop')
-
     model.summary()
-
     model.fit([x_train], [y_train,Y_train_svm], batch_size=batch_size, epochs=epoch_num, verbose=2)
 
+    model = Model(inputs=ip_layer, outputs=[lda_layer])
+    losses = {'lda':lda_trace_loss(n_components, margin)}
+    model.compile(loss=losses, optimizer='rmsprop')
+    model.summary()
+    model.fit([x_train], [y_train], batch_size=batch_size, epochs=epoch_num, verbose=2)
+
+    #losses = {'output':'categorical_crossentropy'}
+
+    
     #model.fit([x_train], [y_train,Y_train_svm], batch_size=batch_size, epochs=epoch_num, shuffle=True, validation_data=(x_test, [y_test,Y_test_svm]),
     #          verbose=2)
 
